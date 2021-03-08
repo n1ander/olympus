74238051c09b15acc832823660c40e951c325822
notdeckvrd <deckvrd@gmail.com>
2021-03-07 19:54:27 -0500

    feat: display json data
    Write a little php code to get json file contents and display the information on a webpage.

--- index.php
+++ index.php
@@ -19,21 +19,22 @@
 		 <table class="table">
 		 	<thead class="thead-dark">
 		 		<tr>
+		 			<th>ID</th>
 		 			<th>Pushups</th>
 		 			<th>Pullups</th>
 		 			<th>Mile Run</th>
 		 		</tr>
 		 	</thead>
 		 	<tbody>
-				<tr>
-					<?php
-					$data = file_get_contents("data.json");
-					$data = json_decode($data, true);
-					echo '<pre>';
-					print_r($data);
-					echo '</pre>';
-					?>
-				</tr>
+				<?php
+				$data = file_get_contents("data.json");
+				$data = json_decode($data, true);
+				foreach($data as $entries){
+					foreach($entries as $entry){
+						echo "<tr><td>" . $entry["id"] . "\t" . $entry["pushups"] . "\t" . $entry["pullups"] . "\t" . $entry["mile"] . "<td><tr>";
+					}
+				}
+				?>
 			</tbody>
 		 </table>
 	</body>
