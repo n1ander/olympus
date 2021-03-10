42277175010f9f04b07a6ce5c560ca9763eced8e
notdeckvrd <deckvrd@gmail.com>
2021-03-10 14:52:23 -0500

    style: reformatting. create separate css file
    Add additional style to table rows. Reformat data.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	<head>
 		<title>Olympus</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<h1>Fitness Logger</h1>
@@ -31,7 +32,7 @@
 				$data = json_decode($data, true);
 				foreach($data as $entries){
 					foreach($entries as $entry){
-						echo "<tr><td>" . $entry["id"] . "\t" . $entry["pushups"] . "\t" . $entry["pullups"] . "\t" . $entry["mile"] . "<td><tr>";
+						echo "<tr><td>" . $entry["id"] . "</td>" . "<td>" . $entry["pullups"] . "</td>" .  "<td>" . $entry["pushups"] . "</td>" .  "<td>" . $entry["mile"] . "</td></tr>";
 					}
 				}
 				?>
