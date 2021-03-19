fa1cc68a8ca07ab77af6e22a3cd2507a67f16e6c
notdeckvrd <deckvrd@gmail.com>
2021-03-19 12:54:23 -0400

    docs: organized files
    Created an includes folder as well as a header file to store header information. Updated index file to include header file too.

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <html>
-	<head>
-		<title>Olympus</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="style.css">
-	</head>
+	<?php include 'includes/header.php'; ?>
 	<body>
 		<h1>Fitness Logger</h1>
 		<br>
