944085a6a89f3ffe4da8cee968e2d6fc58dfc027
notdeckvrd <deckvrd@gmail.com>
2021-03-23 17:28:23 -0400

    docs: create form file
    Remove form from index page and created a new fitness log form page. Included file in the index file.

---
+++ includes/fitness_log_form.php
@@ -0,0 +1,9 @@
+<form action="log.php" method="POST">
+	<label for="push_ups">Pushups:</label>
+	<input type="text" id="push_ups" name="push_ups">
+	<label for="pull_ups">Pullups:</label>
+	<input type="text" id="pull_ups" name="pull_ups">
+	<label for="mile">Mile:</label>
+	<input type="text" id="mile" name="mile">
+	<input type="submit" value="Submit">
+</form>
--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 	<?php include 'includes/header.php'; ?>
 	<body>
 		<h1>Fitness Logger</h1>
-		<br>
-		 <form action="log.php" method="POST">
-		 	<label for="push_ups">Pushups:</label>
-		 	<input type="text" id="push_ups" name="push_ups">
-		 	<label for="pull_ups">Pullups:</label>
-		 	<input type="text" id="pull_ups" name="pull_ups">
-		 	<label for="mile">Mile:</label>
-		 	<input type="text" id="mile" name="mile">
-		 	<input type="submit" value="Submit">
-		 </form>
-		 <br>
-		 <?php include 'includes/fitness_log.php'; ?>
+		<?php include 'includes/fitness_log_form.php'; ?>
+		<?php include 'includes/fitness_log.php'; ?>
 	</body>
 </html>
