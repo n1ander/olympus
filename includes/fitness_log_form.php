3facc9180354b3cd6e4b6e664f503cf1935bf5fe
notdeckvrd <deckvrd@gmail.com>
2021-05-17 12:32:49 -0400

    style: update submit html

--- includes/fitness_log_form.php
+++ includes/fitness_log_form.php
@@ -1,9 +1,9 @@
-<form action="log.php" method="POST">
+<form action="includes/log.php" method="POST">
 	<label for="push_ups">Pushups:</label>
 	<input type="text" id="push_ups" name="push_ups">
 	<label for="pull_ups">Pullups:</label>
 	<input type="text" id="pull_ups" name="pull_ups">
 	<label for="mile">Mile:</label>
 	<input type="text" id="mile" name="mile">
-	<input type="submit" value="Submit">
+	<input type="submit">
 </form>
